203f61892f0e287c4c6ebd8151bd0491a20e4394
DamieBabington <141667828+DamieBabington@users.noreply.github.com>
2023-11-13 20:44:06 +0000

    assessment3 doc

---
+++ my-react-app/src/App.js
@@ -0,0 +1,23 @@
+// App.js
+
+import React, { lazy } from 'react';
+import Useridprofile from 'react';
+
+const app = () =>{
+  const user{
+    name: Beyonce
+    Age:37
+    location:LA
+    imageUrl:'beyonc'
+
+  }
+
+const App = () => {
+  return (
+    <div className="app">
+      <UserProfileCard user={user} />
+    </div>
+  );
+};
+
+export default App;
---
+++ my-react-app/src/UseridProfile.jsx
@@ -0,0 +1,19 @@
+
+function Card (){
+  return (
+    <div className="card">
+<img src={imageUrl} alt={'${beyonce}'s profile'}></img>
+<h2> Beyonce </h2>
+<p>
+  <strong>Age:</strong> {37}
+</p>
+<p>
+  <strong>Location:</strong> {LA}
+  </p>
+    </div>
+  )
+    
+}
+
+
+export default app;
